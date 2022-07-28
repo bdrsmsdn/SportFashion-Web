eb007708cb78385234c66624d98b97b6aa14b350
bdrsmsdn <badrasam7@gmail.com>
2022-07-28 16:20:42 +0700

    First Commit

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCnAd1LVnrEXcVk6vufc2soP69Ecbz_bLw',
+  authDomain: 'jersipedia-2831a.firebaseapp.com',
+  databaseURL: 'https://jersipedia-2831a-default-rtdb.firebaseio.com',
+  projectId: 'jersipedia-2831a',
+  storageBucket: 'jersipedia-2831a.appspot.com',
+  messagingSenderId: '553979924200',
+  appId: '1:553979924200:web:8364505a5915cf2474835d',
+});
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
